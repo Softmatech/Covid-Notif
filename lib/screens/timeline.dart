e1eb036b92ea71d7d38c4791f942af6ff514cd4b
Andy Feidje Joseph <fedjee2007@yahoo.fr>
2020-09-16 13:51:23 -0400

    clipper added

--- lib/screens/timeline.dart
+++ lib/screens/timeline.dart
@@ -18,21 +18,24 @@ class _TimelineState extends State<Timeline> {
       ),
       body: Column(
         children: <Widget>[
-          Container(
-            height: 350,
-            width: double.infinity,
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topRight,
-                    end: Alignment.bottomLeft,
-                    colors: [
-                      kFourthColor,
-                      kPrimaryColor
-                    ]
-                ),
-                image: DecorationImage(
-                    image: AssetImage("assets/images/virus.png")
-                )
+          ClipPath(
+            clipper: MyClipper(),
+            child: Container(
+              height: 350,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                      colors: [
+                        kFourthColor,
+                        kPrimaryColor
+                      ]
+                  ),
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/virus.png")
+                  )
+              ),
             ),
           )
         ],
@@ -59,5 +62,25 @@ class _TimelineState extends State<Timeline> {
     ]);
     super.dispose();
   }
+
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 80);
+    path.quadraticBezierTo(size.width / 2 , size.height, size.width, size.height - 80);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    return false;
+  }
+
 }
 
